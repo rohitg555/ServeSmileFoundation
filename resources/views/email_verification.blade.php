6b029024fa3d04d9b6b03c43a964aaf9b00b3f15
manish shirke <buntyshirke22@gmail.com>
2019-09-28 14:05:01 +0530

    ok

---
+++ resources/views/email_verification.blade.php
@@ -0,0 +1,18 @@
+
+@include('include.header')
+@include('include.app')
+<div class="container">
+	<div class="row">
+<form action="" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email Verification</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
+</body>
+</html>
